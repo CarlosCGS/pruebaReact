8f7c38eab472b2de9be04b71891683d6716a6d5d
carlos garcia santiago <carlosgaziee@gmail.com>
2020-02-03 22:04:04 -0600

    Updates

---
+++ precache-manifest.c3f4444652fa37775c5a29a1b0a89d41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3761ef4a406c45bda81815a34d3ec891",
+    "url": "/pruebaReact/index.html"
+  },
+  {
+    "revision": "345b9542717c3d34f84d",
+    "url": "/pruebaReact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/pruebaReact/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pruebaReact/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345b9542717c3d34f84d",
+    "url": "/pruebaReact/static/js/main.6e779a06.chunk.js"
+  },
+  {
+    "revision": "01323beb4bce538f0a08",
+    "url": "/pruebaReact/static/js/runtime-main.5ae708f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pruebaReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
